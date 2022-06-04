a680d6204a4ae213139ce6e911026ac54edb1097
dhuw <hazlewood.dyl@gmail.com>
2022-06-04 13:44:00 -0700

    added TODO comments
    for reference when working from a different device

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// JavaScript TODOs
+// 1. Add functionality to all buttons
+// 2. get connection to openWeatherMap API
+// 3. set up search functionality
+// 4. make current day highlighted in next 5 days cards
+// 5. implement moment.js for #4

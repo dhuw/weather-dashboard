7fc58320de7c06ad41c9b4b622f65268564684f4
dhuw <hazlewood.dyl@gmail.com>
2022-08-01 21:17:01 -0700

    scriptjs update
    adding elements for the html and form

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,22 @@ $(document).ready(function () {
     //api key
     const apiKey = 'efcf0b46c6efbbb9f3196849ae5cef6a';
 
+    //html el
+    const cityEl = $('h2#city');
+    const dateEl = $('h3#date');
+    const weatherIconEl = $('img#weather-icon');
+    const temperatureEl = $('span#temperature');
+    const humidityEl = $('span#humidity');
+    const windEl = $('span#wind');
+    const uvIndexEl = $('span#uv-index');
+    const cityListEl = $('div.cityList');
 
+
+    //form
+    const cityInput = $('#city-input');
+
+    //recent search btns
+ 
 
 
 
